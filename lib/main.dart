2c7bead4fc4b3fa49082dc8cdfda5f072dc6d571
dnth <dickson.neoh@gmail.com>
2021-12-09 22:40:48 +0800

    add working slider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_microalgae/detect_image.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
@@ -10,13 +11,19 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 final List<String> imgList = [
-  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  'https://github.com/dnth/flutter_microalgae/blob/main/sample_images/IMG_20191212_151351.jpg?raw=true',
+  'https://github.com/dnth/flutter_microalgae/blob/main/sample_images/IMG_20191212_151438.jpg?raw=true',
+  'https://github.com/dnth/flutter_microalgae/blob/main/sample_images/IMG_20191212_151844.jpg?raw=true',
+  'https://github.com/dnth/flutter_microalgae/blob/main/sample_images/IMG_20191212_153614.jpg?raw=true'
 ];
+
+// Reading bytes from a network image
+Future<Uint8List> readNetworkImage(String imageUrl) async {
+  final ByteData data =
+      await NetworkAssetBundle(Uri.parse(imageUrl)).load(imageUrl);
+  final Uint8List bytes = data.buffer.asUint8List();
+  return bytes;
+}
 
 void main() {
   runApp(const MyApp());
@@ -53,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       RoundedLoadingButtonController();
 
   // File? imageURI;
-  String? imageURIWeb;
+  // String? imageURIWeb;
   Uint8List? imageMemory;
   Uint8List? imgBytes;
   bool isClassifying = false;
@@ -85,50 +92,63 @@ class _MyHomePageState extends State<MyHomePage> {
     return croppedFile!;
   }
 
-  final List<Widget> imageSliders = imgList
-      .map((item) => Container(
-            child: Container(
-              margin: const EdgeInsets.all(5.0),
-              child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-                  child: Stack(
-                    children: <Widget>[
-                      Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                      Positioned(
-                        bottom: 0.0,
-                        left: 0.0,
-                        right: 0.0,
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                Color.fromARGB(200, 0, 0, 0),
-                                Color.fromARGB(0, 0, 0, 0)
-                              ],
-                              begin: Alignment.bottomCenter,
-                              end: Alignment.topCenter,
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> imageSliders = imgList
+        .map((item) => Container(
+              child: Container(
+                margin: const EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        GestureDetector(
+                            onTap: () async {
+                              String imgUrl = imgList[imgList.indexOf(item)];
+
+                              final bytes = await readNetworkImage(imgUrl);
+
+                              setState(() {
+                                imageMemory = bytes;
+                              });
+
+                              print("Tapped on image ${imgList.indexOf(item)}");
+                            },
+                            child: Image.network(item,
+                                fit: BoxFit.cover, height: 1000.0)),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
                             ),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 20.0),
-                          child: Text(
-                            'No. ${imgList.indexOf(item)} image',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Text(
+                              'No. ${imgList.indexOf(item)} image',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  )),
-            ),
-          ))
-      .toList();
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -141,6 +161,7 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               CarouselSlider(
                 options: CarouselOptions(
+                  height: 200,
                   autoPlay: true,
                   aspectRatio: 2.0,
                   enlargeCenterPage: true,
@@ -180,11 +201,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           isClassifying = true;
                         });
 
-                        if (kIsWeb) {
-                          imgBytes = imageMemory;
-                        } else {
-                          imgBytes = imageMemory;
-                        }
+                        imgBytes = imageMemory;
 
                         String base64Image =
                             "data:image/png;base64," + base64Encode(imgBytes!);
